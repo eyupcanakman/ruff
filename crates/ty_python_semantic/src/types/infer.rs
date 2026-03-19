432a31ce6b19878b4c93ce49125d3c4eede72c3f
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2026-03-19 15:04:30 -0400

    fix cycle handling pollution for use constraints

--- crates/ty_python_semantic/src/types/infer.rs
+++ crates/ty_python_semantic/src/types/infer.rs
@@ -595,7 +595,9 @@ impl<'db> ScopeInference<'db> {
                     .collect();
             }
         }
-        if let Some(prev_extra) = &previous_inference.extra {
+        if cycle.iteration() > 1
+            && let Some(prev_extra) = &previous_inference.extra
+        {
             for (def, prev_types) in &prev_extra.use_contexts {
                 let extra = self.extra.get_or_insert_default();
 
@@ -770,7 +772,9 @@ impl<'db> DefinitionInference<'db> {
                     .collect();
             }
         }
-        if let Some(prev_extra) = &previous_inference.extra {
+        if cycle.iteration() > 1
+            && let Some(prev_extra) = &previous_inference.extra
+        {
             for (def, prev_types) in &prev_extra.use_contexts {
                 let extra = self.extra.get_or_insert_default();
 
@@ -936,7 +940,9 @@ impl<'db> ExpressionInference<'db> {
                     .map(|ty| ty.recursive_type_normalized(db, cycle))
                     .collect();
             }
-            if let Some(prev_extra) = &previous.extra {
+            if cycle.iteration() > 1
+                && let Some(prev_extra) = &previous.extra
+            {
                 for (def, prev_types) in &prev_extra.use_contexts {
                     let extra = self.extra.get_or_insert_default();
 
